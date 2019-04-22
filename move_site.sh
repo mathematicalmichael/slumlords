1f75366781cdecf803bae0963d4b79347f2f251d
Michael Pilosov <consistentbayes@gmail.com>
2019-04-22 07:01:13 -0600

    updated moving instructions.

--- move_site.sh
+++ move_site.sh
@@ -2,9 +2,12 @@
 hugo
 git stash
 git checkout gh-pages
+# pull remote changes to avoid conflicts
+git pull
 # hide public 
 mv public .public
-# so remove ignores it
+mv CNAME .CNAME
+# remove ignores hidden files
 rm -r *
 # unpack hidden public
 mv .public/* .
@@ -13,6 +16,7 @@ git add *
 # auto-commit them
 git commit -am 'new pages auto-build.'
 rm -r .public
+mv .CNAME CNAME
 git push origin gh-pages
 git checkout master
 git stash pop
